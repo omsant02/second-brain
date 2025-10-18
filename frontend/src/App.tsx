f6535cfb300fe0eb129b945383e1e5510fe9fc1c
om santoshwar <santo@oms-MacBook-Pro.local>
2025-10-18 16:10:32 +0530

    create button comp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import './App.css'
-import { Button } from './components/ui/Button'
+import "./App.css";
+import { Button } from "./components/ui/Button";
+import { PlusIcon } from "./icons/PlusIcon";
+import { ShareIcon } from "./icons/ShareIcoc";
 
 function App() {
-
   return (
     <div>
       hi there
-    <Button variant="primary" size="md" onclick={() => {}} text="asd" />
+      <Button
+        startIcon={<PlusIcon size="md" />}
+        variant="primary"
+        size="sm"
+        onclick={() => {}}
+        text="Share"
+      />
+      <Button startIcon={<ShareIcon size="md" />} variant="secondary" size="sm" onclick={() => {}} text="Share" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,14 +1,39 @@
+import type { ReactElement } from "react";
+
+type Variants = "primary" | "secondary";
+
 export interface ButtonProps {
-    variant: "primary" | "secondary";
-    size: "sm" | "md" | "lg";
-    text: string;
-    startIcon?: any;
-    endIcon?: any;
-    onclick: () => void
+  variant: Variants;
+  size: "sm" | "md" | "lg";
+  text: string;
+  startIcon?: ReactElement;
+  endIcon?: ReactElement;
+  onclick: () => void;
 }
+
+const variantStyles = {
+  primary: "bg-purple-600 text-white",
+  secondary: "bg-purple-300 text-purple-600",
+};
+
+const sizeStyles = {
+  sm: "py-1 px-2",
+  md: "py-2 px-4",
+  lg: "py-4 px-6",
+};
+
+const defaultStyles = "rounded-md p-1 flex";
 
 export const Button = (props: ButtonProps) => {
-    return <button>click here</button>
-}
+  return (
+    <button
+      className={`${variantStyles[props.variant]} ${defaultStyles} ${
+        sizeStyles[props.size]
+      }`}
+    >
+      {props.startIcon ? <div className="pr-2 pt-1">{props.startIcon}</div> : null} {props.text} {props.endIcon}
+    </button>
+  );
+};
 
-<Button variant="primary" size="md" onclick={() => {}} text="asd" />
+<Button variant="primary" size="md" onclick={() => {}} text="asd" />;
