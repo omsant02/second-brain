7fa494009f1a015969892fee5f0dc049182d21d8
om santoshwar <santo@oms-MacBook-Pro.local>
2025-10-20 00:04:04 +0530

    add style in sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,14 +1,20 @@
+import { Logo } from "../icons/Logo";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
 import { SidebarItem } from "./SidebarItem";
 
 export function Sidebar() {
-    return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 pl-6">
-        <div className="flex text-2xl pt-4">
-            Brainly
+  return (
+    <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 pl-6">
+      <div className="flex text-2xl pt-8 items-center">
+        <div className="pr-2 text-purple-700">
+          <Logo />
         </div>
-        <div className="pt-4">
-            <SidebarItem text="Youtube" icon={<YoutubeIcon/>}/>
-            <SidebarItem text="Youtube" icon={<YoutubeIcon/>}/>
-        </div>
+        Brainly
+      </div>
+      <div className="pt-8 pl-4">
+        <SidebarItem text="Youtube" icon={<YoutubeIcon />} />
+        <SidebarItem text="Youtube" icon={<YoutubeIcon />} />
+      </div>
     </div>
+  );
 }
--- frontend/src/components/SidebarItem.tsx
+++ frontend/src/components/SidebarItem.tsx
@@ -8,7 +8,7 @@ export function SidebarItem({
   icon: ReactElement;
 }) {
   return (
-    <div className="flex text-gray-700 py-2">
+    <div className="flex text-gray-700 py-2 cursor-pointer hover:bg-gray-200 rounded max-w-48 pl-4 transition-all duration-150">
       <div className="pr-2">{icon}</div>
       <div className="">{text}</div>
     </div>
