2fc2ce3eee8128dcc3fc8a99e4056798f9c1bc4d
om santoshwar <santo@oms-MacBook-Pro.local>
2025-10-19 23:53:22 +0530

    create sidebar items

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,5 +1,14 @@
+import { YoutubeIcon } from "../icons/YoutubeIcon";
+import { SidebarItem } from "./SidebarItem";
+
 export function Sidebar() {
-    return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0">
-        hi there
+    return <div className="h-screen bg-white border-r w-72 fixed left-0 top-0 pl-6">
+        <div className="flex text-2xl pt-4">
+            Brainly
+        </div>
+        <div className="pt-4">
+            <SidebarItem text="Youtube" icon={<YoutubeIcon/>}/>
+            <SidebarItem text="Youtube" icon={<YoutubeIcon/>}/>
+        </div>
     </div>
 }
---
+++ frontend/src/components/SidebarItem.tsx
@@ -0,0 +1,16 @@
+import type { ReactElement } from "react";
+
+export function SidebarItem({
+  text,
+  icon,
+}: {
+  text: string;
+  icon: ReactElement;
+}) {
+  return (
+    <div className="flex text-gray-700 py-2">
+      <div className="pr-2">{icon}</div>
+      <div className="">{text}</div>
+    </div>
+  );
+}
